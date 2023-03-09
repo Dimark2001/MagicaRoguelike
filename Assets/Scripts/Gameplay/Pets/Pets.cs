1857c03cc868987157e80f056ddd7cc99565e4ed
DmitryK <>
2023-03-09 20:13:19 +0500

    add pets

---
+++ Assets/Scripts/Gameplay/Pets/Pets.cs
@@ -0,0 +1,70 @@
+using System;
+using DG.Tweening;
+using Gameplay.Character;
+using Gameplay.Weapon;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pets : BaseCharacter
+{
+    [SerializeField] private CharacterMovement characterMovement;
+    [SerializeField] private float rad;
+    private bool _isCanAttack;
+
+    private void Awake()
+    {
+        _isCanAttack = true;
+        rb = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void TakeDamage(int dmg, DamageType type, Weapon source)
+    {
+        
+    }
+
+    public override void KnockBack(Vector3 dir, float force)
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        if(LevelManager.Instance.player == null) return;
+        
+        characterMovement.MovementToTheSelectionPosition(LevelManager.Instance.player.transform.position, navMeshAgent.stoppingDistance, navMeshAgent);
+        if (_isCanAttack)
+        {
+            if (FindEnemyInRadius() != null)
+            {
+                UseAbility(FindEnemyInRadius());
+            }
+        }
+    }
+    
+    protected Transform FindEnemyInRadius()
+    {
+        var hitColliders = Physics.OverlapSphere(transform.position, rad);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.TryGetComponent(out Enemy enemy))
+            {
+                return enemy.transform;
+            }
+        }
+
+        return null;
+    }
+
+    protected virtual void UseAbility(Transform enemy)
+    {
+        print(enemy.name);
+        _isCanAttack = false;
+        var inVal = 0f;
+        DOTween.To(() => inVal, x => inVal = x, 1, attackCooldown).OnComplete(() =>
+        {
+            _isCanAttack = true;
+        });
+    }
+}
